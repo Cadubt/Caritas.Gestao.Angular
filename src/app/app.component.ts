faf79693490cb127a0adca0d5e2cf9fbcc09d4b3
Rodrigo Almeida <rod.almeida01@gmail.com>
2020-10-23 15:34:08 -0300

    C2 - card width and tool bar adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from './pages/login/auth.service';
 
 @Component({
@@ -6,17 +6,26 @@ import { AuthService } from './pages/login/auth.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
   title = 'Caritas Gestão';
-  showMenu;
+  showMenu = true
 
   constructor (
     private authService: AuthService,
+    private changeDetectorRef: ChangeDetectorRef
   ) { }
 
   ngOnInit(){
     this.authService.showMenuEmitter.subscribe(
       show => this.showMenu = show
     )
+    this.changeDetectorRef.detectChanges();
+    // this.showMenu = this.authService.isMenuShowing
+  }
+  ngAfterViewChecked(){
+    this.authService.showMenuEmitter.subscribe(
+      show => this.showMenu = show
+    )
+    this.changeDetectorRef.detectChanges();
   }
 }
