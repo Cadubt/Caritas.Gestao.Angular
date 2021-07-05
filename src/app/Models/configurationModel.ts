d97d7635e556084c6af2b9afda3eb0f9add0cfa1
Cadu <cadubt@gmail.com>
2021-07-05 18:07:28 -0300

    creating configuration page

---
+++ src/app/Models/configurationModel.ts
@@ -0,0 +1,9 @@
+export class configurationModel{
+    public id: number;
+    public name: string;
+    public password: string;
+    public email: string;
+    public role: number;
+    public createdAt: string;
+    public deletedAt: string;
+}
---
+++ src/app/Pages/configuration/configuration.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ConfigurationComponent } from './configuration.component';
+
+
+
+@NgModule({
+  declarations: [ConfigurationComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ConfigurationModule { }
