bdbc3662e97bad90c90d840e750a1379d4ea402e
Cadu <cadubt@gmail.com>
2021-07-07 16:58:28 -0300

    User configuration page bugs fixed

--- src/app/Core/user-configuration.service.ts
+++ src/app/Core/user-configuration.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
+const API_URL = environment.BASE_URL_API;
 @Injectable({
   providedIn: 'root'
 })
-export class ConfigurationService {
+export class UserConfigurationService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUserById(){
+    var urlString=`${API_URL}User/GetUserById`;
+    return this.http.get(urlString)
+  }
+
 }
+
